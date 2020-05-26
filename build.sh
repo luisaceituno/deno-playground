04c2b165030079e97fec5bcd8e9d7ffc0373dab3
Luis Aceituno <lmaceituno92@gmail.com>
2020-05-26 23:47:10 +0200

    add log when deno found

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ export PATH="$DENO_INSTALL/bin:$PATH"
 
 if ! [ -x "$(command -v deno)" ]; then
   curl -fsSL https://deno.land/x/install/install.sh | sh
+else
+  echo "Found deno installation. Skipping install."
 fi
 
